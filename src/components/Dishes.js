32d6d9e93fd505ae24f176c41993b2af56263c99
girikanchan <akira.giri2001@gmail.com>
2023-11-16 14:08:04 +0530

    css responsive

--- src/components/Dishes.js
+++ src/components/Dishes.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './dishes.css';
 
-
 function Dishes({ dishes }) {
   return (
-    <div className='DishesSection'>
-      <div className='DishesList'>
-        {dishes.map((dish, index) => (
-          <div key={index} className='DishContainer'>
-            <div className='DishesDetail'>
-              <p>${dish.price}</p>
-              <h5>{dish.name}</h5>
-              <p>{dish.description}</p>
+    <div className='back'>
+      <div className='DishesSection'>
+        <div className='DishesList'>
+          {dishes.map((dish, index) => (
+            <div key={index} className='DishContainer'>
+              <div className='DishImage'>
+                <img src={dish.image} alt={dish.name} />
+              </div>
+              <div className='DishesDetail'>
+                <h5>{dish.name}</h5>
+                <p id='price'>${dish.price}</p>
+                <p>{dish.description}</p>
+              </div>
             </div>
-            <div className='DishImage'>
-              <img src={dish.image} alt={dish.name} />
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
